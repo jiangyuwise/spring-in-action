3c75707386d71dac0aea96ef3dd3cb3f57002b4b
jiangyu <jiangyuwise@gmail.com>
2019-10-28 18:30:07 +0800

    使用 Qualifier 注解

---
+++ ch03/03-qualifier/src/main/java/com/codve/Group.java
@@ -0,0 +1,26 @@
+package com.codve;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author admin
+ * @date 2019/10/28 17:38
+ */
+@Component
+public class Group {
+
+    private Person person;
+
+    @Autowired
+    @Qualifier("student")
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public void info() {
+        person.info();
+    }
+}
---
+++ ch03/03-qualifier/src/main/java/com/codve/PersonConfig.java
@@ -0,0 +1,28 @@
+package com.codve;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * 使用 Qualifier 解决冲突
+ * @author admin
+ * @date 2019/10/28 17:36
+ */
+@Configuration
+@ComponentScan
+public class PersonConfig {
+
+    @Bean
+    public Person employee() {
+        return new Employee("Jimmy", "developer");
+    }
+
+    @Bean
+    @Qualifier("student")
+    public Person student() {
+        return new Student("James", "CS");
+    }
+}
