1ba9886ad3f86a10c3b1f4c0f8640e1cd3048962
jiangyu <jiangyuwise@gmail.com>
2019-11-07 18:47:27 +0800

    学习 nested

---
+++ ch10/11-propagation-nested/src/main/java/com/codve/ArticleServiceImpl.java
@@ -0,0 +1,48 @@
+package com.codve;
+
+import org.omg.SendingContext.RunTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author admin
+ * @date 2019/11/7 11:49
+ */
+@Service
+@Transactional
+public class ArticleServiceImpl implements ArticleService {
+
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    public void setArticleRepository(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NESTED)
+    public Article save(Article article) {
+        return articleRepository.save(article);
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NESTED)
+    public void saveWithException(Article article) {
+        articleRepository.save(article);
+        throw new RuntimeException();
+    }
+
+    @Override
+    public List<Article> findByUserId(Long userId) {
+        return articleRepository.findByUserId(userId);
+    }
+
+    @Override
+    public List<Article> findByTitle(String title) {
+        return articleRepository.findByTitle(title);
+    }
+}
---
+++ ch10/11-propagation-nested/src/main/java/com/codve/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.codve;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author admin
+ * @date 2019/11/6 18:28
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NESTED)
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        Iterable<User> userIterable = userRepository.findAll();
+        List<User> userList = new ArrayList<>();
+        userIterable.forEach(userList::add);
+        return userList;
+    }
+
+    @Override
+    public List<User> findByName(String name) {
+        return userRepository.findByName(name);
+    }
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userOptional.orElse(null);
+    }
+}
