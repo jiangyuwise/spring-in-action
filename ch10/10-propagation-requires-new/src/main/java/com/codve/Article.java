a931feadc02ea934edb014a21a3ed62fb9a782fd
jiangyu <jiangyuwise@gmail.com>
2019-11-07 18:25:52 +0800

    学习 requires_new

---
+++ ch10/10-propagation-requires-new/src/main/java/com/codve/Article.java
@@ -0,0 +1,41 @@
+package com.codve;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * @author admin
+ * @date 2019/11/5 11:11
+ */
+@Data
+@Entity
+@Table(name = "article")
+public class Article {
+
+    @Id
+    @Column(name = "article_id")
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "article_title")
+    private String title;
+
+    @Column(name = "create_time")
+    private long createTime;
+
+    @Override
+    public String toString() {
+        Date date = new Date(createTime);
+        return "id: " + id
+                + ", userId: " + userId
+                + ", article_title: " + title
+                + ", crete_time: " + String.format("%tF", date);
+    }
+}
