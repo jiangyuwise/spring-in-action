b5b3e2a7f564ab38d55fbfd9cf60a576c2c01019
jiangyu <jiangyuwise@gmail.com>
2019-11-08 14:25:32 +0800

    使用 logback

---
+++ ch01/logback/src/main/java/com/codve/App.java
@@ -0,0 +1,19 @@
+package com.codve;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author admin
+ * @date 2019/11/8 13:23
+ */
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("hello, world");
+        logger.info("{}", "hello, world");
+
+    }
+}
