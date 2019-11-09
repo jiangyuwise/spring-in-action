95a304cf9745aef74f95ccd6002914ecf6fe9f5d
jiangyu <jiangyuwise@gmail.com>
2019-11-09 23:05:48 +0800

    重构老代码

--- ch03/04-qualifier/src/test/java/com/codve/PersonConfigTest.java
+++ ch03/04-qualifier/src/test/java/com/codve/PersonConfigTest.java
@@ -1,18 +1,18 @@
 package com.codve;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author admin
  * @date 2019/10/28 17:39
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = PersonConfig.class)
 public class PersonConfigTest {
 
