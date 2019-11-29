4b69a1d651e702d96ef1443047687e4e429be3a3
jiangyu <jiangyuwise@gmail.com>
2019-11-29 09:55:45 +0800

    优化底层

--- ch13/04-mybatis/src/main/java/com/codve/mybatis/dao/ArticleMapper.java
+++ ch13/04-mybatis/src/main/java/com/codve/mybatis/dao/ArticleMapper.java
@@ -13,15 +13,45 @@ import java.util.List;
 @Repository
 public interface ArticleMapper {
 
+    /**
+     * 保存 articleDO
+     * @param articleDO articleDO
+     * @return int
+     */
     int save(ArticleDO articleDO);
 
+    /**
+     * 根据 id 删除
+     * @param id id
+     * @return int
+     */
     int deleteById(Long id);
 
+    /**
+     * 更新一条记录
+     * @param articleDO articleDO
+     * @return int
+     */
     int update(ArticleDO articleDO);
 
+    /**
+     * 根据 id 查找记录
+     * @param id id
+     * @return articleDO
+     */
     ArticleDO findById(Long id);
 
+    /**
+     * 按条件查找 articleDO
+     * @param articleQuery 查询条件
+     * @return List<ArticleDO>
+     */
     List<ArticleDO> find(ArticleQuery articleQuery);
 
+    /**
+     * 按条件查找 articleDO 的数量
+     * @param articleQuery 查询条件
+     * @return int
+     */
     int count(ArticleQuery articleQuery);
 }
--- ch13/04-mybatis/src/main/java/com/codve/mybatis/dao/UserMapper.java
+++ ch13/04-mybatis/src/main/java/com/codve/mybatis/dao/UserMapper.java
@@ -13,15 +13,45 @@ import java.util.List;
 @Repository
 public interface UserMapper {
 
+    /**
+     * 保存
+     * @param userDO userDO
+     * @return int
+     */
     int save(UserDO userDO);
 
-    int deleteById(Long userId);
+    /**
+     * 根据 id 删除
+     * @param id id
+     * @return int
+     */
+    int deleteById(Long id);
 
+    /**
+     * 更新
+     * @param userDO userDO
+     * @return int
+     */
     int update(UserDO userDO);
 
+    /**
+     * 根据 id 查找一条记录
+     * @param id id
+     * @return userDO
+     */
     UserDO findById(Long id);
 
+    /**
+     * 根据条件查找记录
+     * @param userQuery 查询条件
+     * @return List<UserDO>
+     */
     List<UserDO> find(UserQuery userQuery);
 
+    /**
+     * 根据条件统计条数
+     * @param userQuery 查询条件
+     * @return int
+     */
     int count(UserQuery userQuery);
 }
